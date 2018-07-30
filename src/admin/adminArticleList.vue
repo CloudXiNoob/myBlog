f05f4668c9e3765fcdc89528469a9d1f5cfb3466
sobrown <zxrnhy1993@sina.com>
2018-07-30 16:07:50 +0800

    修改列表页面

--- src/admin/adminArticleList.vue
+++ src/admin/adminArticleList.vue
@@ -1,19 +1,20 @@
 <template lang="html">
     <div>
-        <el-table border style="width:100%;" :default-sort="{prop:'createTime',order:'descending'}" :data="tableData">
-          <el-table-column label="编号" prop="_id" sortable width="150" align="center"></el-table-column>
+        <el-table border style="width:100%;" :default-sort="{prop:'createTime',order:'descending'}" :data="tableData" v-loading="loading">
+          <el-table-column type="index" width="150" align="center"></el-table-column>
           <el-table-column label="标题" prop="title"  width="90" align="center"></el-table-column>
           <el-table-column label="描述" prop="desc"  width="150" align="center"></el-table-column>
           <el-table-column label="标签" prop="tag"  width="70" align="center"></el-table-column>
           <el-table-column label="发布时间" prop="createTime"  width="180" align="center"></el-table-column>
           <el-table-column label="内容" prop="content" align="center"></el-table-column>
           <el-table-column label="操作"  width="210" align="center">
-              <template scope="scoped">
-                  <el-button type="info" size="small">查看/编辑</el-button>
-                  <el-button type="danner" size="small">删除</el-button>
+              <template slot-scope="scope">
+                  <el-button type="info" size="small" @click="handle(scope.$index,scope.row)" >查看/编辑</el-button>
+                  <el-button type="danner" size="small" @click="handleDelete(scope.$index,scope.row)">删除</el-button>
               </template>
           </el-table-column>
         </el-table>
+        <el-pagination layout="prev, pager, next"  :total="10" background></el-pagination>
     </div>
 </template>
 <script>            
@@ -23,7 +24,8 @@ export default {
         return {
             pageSize:8,
             page:1,
-            tableData:[]
+            tableData:[],
+            loading:true
         }
     },
     methods: {
@@ -36,10 +38,37 @@ export default {
             axios.post('http://localhost:3000/api/article/getAll',param).then((res) => {
                 if(res.data.success === true){
                     this.tableData =res.data.articleArr
+                    this.loading = false;
                 }
             })
         },
-
+        handleDelete(index){
+            console.log(index);
+            let id =  this.tableData[index]._id;
+            console.log(id);
+            this.$confirm('将删除该文章，是否继续','警告',{
+                confirmButtonText:'确定',
+                cancelButtonText:'取消',
+                type:'warning'
+            }).then(()=> {
+                axios.post("http://localhost:3000/api/article/delete/" + id,{})
+                .then((res) => {
+                   if(res.data.success===true){
+                       this.$message({
+                           type:'success',
+                           message:'文章已删除'
+                       })
+                       this.init()
+                   }else{
+                       this.$message.error('删除不成功');
+                   }
+                })
+            })
+        },
+        handle (index) {
+            let articleId = this.tableData[index]._id;
+            this.$router.push({path:'/admin/change',query:{articleId:articleId }})
+        }
     },
     mounted () {
         this.init()
